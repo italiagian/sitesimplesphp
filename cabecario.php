d4208af653f0ee5f3fe976f6ff89854ad76dff78
giancarlo poltronieri <giancarlo@ducaleimoveis.com.br>
2014-08-31 22:18:33 -0300

    acerto em arquivos que faltavam

--- cabecario.php
+++ cabecario.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt_BR">
   <head>
-    <meta charset="utf-8">
+    
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
     <title>Site Simples em PHP</title>
+    
 
    <!-- Custom styles for this template -->
    <link href="starter-template.css" rel="stylesheet">
---
+++ includes/erro.php
@@ -0,0 +1,13 @@
+
+<div class="jumbotron">
+  <div class="container">
+    <div class="starter-template">
+        <h1> AVISO !</h1>
+        <div class="hero-unit ">
+          <h1>Página Não encontrada <small><font face="Tahoma" color="red">Error 404</font></small></h1>
+          <br />
+         
+        </div>
+  </div>
+</div>
+</div>
